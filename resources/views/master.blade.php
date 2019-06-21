80a2fc4f4699c68498d284c02f2aa007df6c3579
Raka Fajar <rakafajar18@gmail.com>
2019-06-21 22:33:49 +0700

    Revisi MAster Blade

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -115,6 +115,7 @@
           <a class="dropdown-item" href="{{ route('kelurahan.index') }}">Data Desa</a>
           <a class="dropdown-item" href="{{ route('agama.index') }}">Data Agama</a>
           <a class="dropdown-item" href="{{ route('pendidikan.index') }}">Data Pendidikan</a>
+          <a class="dropdown-item" href="{{ route('jenispekerjaan.index') }}">Data Pekerjaan</a>
           <a class="dropdown-item" href="{{ route('perkawinan.index') }}">Data Perkawinan</a>
           <a class="dropdown-item" href="{{ route('hubungankeluarga.index') }}">Hubungan Keluarga</a>
         </div>
