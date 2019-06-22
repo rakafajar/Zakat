73a449ac63de4e24e636bd0bde38dc6c4c0e71fb
Raka Fajar <rakafajar18@gmail.com>
2019-06-22 16:23:03 +0700

    Bikin Menu Kembali

--- resources/views/anggotakeluarga/create.blade.php
+++ resources/views/anggotakeluarga/create.blade.php
@@ -118,7 +118,7 @@
           </div>
           <button type="submit" class="btn btn-info btn-sm"><i class="fas fa-save"></i> Simpan</button>
           <button type="reset" class="btn btn-warning btn-sm"><i class="fas fa-redo-alt"></i> Reset</button>
-          <a href="" class="btn btn-danger btn-sm"><i class="fas fa-arrow-circle-left"></i> Kembali</a>
+          <a href="{{ route('anggotakeluarga.index') }}" class="btn btn-danger btn-sm"><i class="fas fa-arrow-circle-left"></i> Kembali</a>
         </form>
       </div>    
     </div>
