78a90e9757d25f7fc689afba1fc16ca68b937a6a
vnurhaqiqi <viqinurhaqiqi@gmail.com>
2019-06-24 11:24:46 +0700

    tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -55,14 +55,9 @@
             </div>
             <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
+                    <button type="submit" class="btn btn-info">
                         {{ __('Login') }}
                     </button>
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="#">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
                 </div>
             </div>
         </form>
